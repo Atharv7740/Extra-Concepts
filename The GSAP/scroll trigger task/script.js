6f80fa45b2b83247f6a3168931b0056217330da4
Atharv7740 <atharv7740@gmail.com>
2024-09-11 21:43:41 +0530

    update

---
+++ The GSAP/scroll trigger task/script.js
@@ -0,0 +1,13 @@
+gsap.to("#page-2 h1",{
+    x:"-130%",
+    scrollTrigger:{
+        trigger:"#page-2",
+        scroller:"body",
+        start:"top 0",
+        end:"top -100%",
+        scrub:3,
+        pin:true,
+
+    }
+
+})
